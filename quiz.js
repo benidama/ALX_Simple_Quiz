bdb3ea8bfea3ea38d0d3e37f758bd58f0685ea92
benidama <benidama02@gmail.comls>
2024-10-06 10:28:39 +0000

    chanded

--- quiz.js
+++ quiz.js
@@ -1,7 +1,9 @@
 function checkAnswer() {
   let correctAnswer = "4";
   let userAnswer = document.querySelector("input").value.checked;
-  if (correctAnswer === userAnswer) {
+  const names = document.querySelector("quiz");
+  const answer = document.querySelector("#choice1").value.checked;
+  if (correctAnswer === userAnswer && correctAnswer === answer) {
     document.getElementById("feedback").innerHTML = "Correct! Well done.";
   } else {
     document.getElementById("feedback").innerHTML =
